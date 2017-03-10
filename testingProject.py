d007bf6fe767f11d1261354e404609ec54415794
MishkaEchantelle <echantelle.mishka@gmail.com>
2017-03-10 15:11:09 +0300

    updated files

--- testingProject.py
+++ testingProject.py
@@ -1,6 +1,12 @@
 import unittest
-from project import project
+# from project import project
 
-class testProject(Testcase)
-	def TestNone(self):
-		self.assertTrue(project)
+class testProject(unittest.Testcase):
+
+	def  test_add_skills(self):
+		self.assertTrue(project(None), msg='Please add a course:')
+	def test_view_skills(self):
+		self.assertTrue(project(None), msg='Please complete a course:')
+	
+	if __name__ == '__main__':
+		unittest.main()
